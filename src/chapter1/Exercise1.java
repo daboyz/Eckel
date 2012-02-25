8c4c43dd78fecf81716f65c744f181c5018ab730
Anton Shashuk <humaninterfaced@gmail.com>
2012-02-25 11:24:24 +0200

    ch1 ex1-2

---
+++ src/chapter1/Exercise1.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+public class Exercise1 {
+	static int i;
+	static char ch;
+	public static void main (String[] args){
+	    System.out.println("Integer: " + i);
+		System.out.println("Character: " + ch); // why there's no character output?
+   }
+}
---
+++ src/chapter1/Exercise2.java
@@ -0,0 +1,7 @@
+package chapter1;
+
+public class Exercise2 {
+   public static void main (String [] args){
+      System.out.println("Hello World");
+   }
+}
