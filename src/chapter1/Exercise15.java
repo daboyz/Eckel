8306518dd4f03b486f9c412f09fdbd575d2c681a
Anton Shashuk <humaninterfaced@gmail.com>
2012-02-26 02:33:17 +0200

    ch1 ex15+html_docs

---
+++ src/chapter1/Exercise15.java
@@ -0,0 +1,19 @@
+package chapter1;
+//: chapter1/Exercise15.java
+/** My <b>first</b> Java program.
+* Displays a <em>Hello world</em> string.
+* @author Anton Shashuk
+* @version 1.0
+*/
+public class Exercise15 {
+	/** A very simple method it is:
+	 * @see Exercise15
+	* @param args array of string arguments
+	* @throws exceptions No exceptions thrown
+	*/
+	  public static void main (String [] args){
+	      System.out.println("Hello World");
+	  }
+}/* Output: (100% match)
+Hello World
+*///:~
