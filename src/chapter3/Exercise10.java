f11d8a8543866e7f4e5628c88e3cced8f3a25c69
Anton Shashuk <humaninterfaced@gmail.com>
2012-03-10 17:45:24 +0200

    ch3 ex6-10

---
+++ src/chapter3/Exercise10.java
@@ -0,0 +1,31 @@
+package chapter3;
+/* Exercise 10: A vampire number has an even number of digits and is formed by multiplying a
+ * pair of numbers containing half the number of digits of the result. 
+ * The digits are taken from the original number in any order. Pairs of trailing zeroes are not allowed. Examples include:
+ * 1260 = 21 * 60
+ * 1827 = 21 * 87
+ * 2187 = 27 * 81
+ * Write a program that finds all the 4-digit vampire numbers.
+ */
+public class Exercise10 {
+	public static void main(String[] args){
+		for (int i=100; i<10000; i++){
+				int i1 = i/1000, i2 = (i%1000)/100, i3 = (i%1000)%100/10, i4 = ((i%1000)%100)%10;
+				if ((i1*10+i2)*(i3*10+i4)==i) System.out.println(i + " = " + i1 + i2 + " * " + i3 + i4);
+				else if ((i2*10+i1)*(i3*10+i4)==i) System.out.println(i + " = " + i1 + i2 + " * " + i3 + i4);
+				else if ((i2*10+i1)*(i3*10+i4)==i) System.out.println(i + " = " + i2 + i1 + " * " + i3 + i4);
+				else if ((i1*10+i2)*(i4*10+i3)==i) System.out.println(i + " = " + i1 + i2 + " * " + i4 + i3);
+				else if ((i2*10+i1)*(i4*10+i3)==i) System.out.println(i + " = " + i2 + i1 + " * " + i4 + i3);
+				else if ((i2*10+i1)*(i4*10+i3)==i) System.out.println(i + " = " + i2 + i1 + " * " + i3 + i4);
+				else if ((i3*10+i2)*(i1*10+i4)==i) System.out.println(i + " = " + i3 + i2 + " * " + i1 + i4);
+				else if ((i3*10+i2)*(i1*10+i4)==i) System.out.println(i + " = " + i3 + i2 + " * " + i1 + i4);
+				else if ((i3*10+i1)*(i2*10+i4)==i) System.out.println(i + " = " + i3 + i1 + " * " + i2 + i4);
+				else if ((i3*10+i1)*(i4*10+i2)==i) System.out.println(i + " = " + i3 + i1 + " * " + i4 + i2);
+				else if ((i4*10+i3)*(i2*10+i1)==i) System.out.println(i + " = " + i4 + i3 + " * " + i2 + i1);
+				else if ((i4*10+i3)*(i1*10+i2)==i) System.out.println(i + " = " + i4 + i3 + " * " + i1 + i2);
+				else if ((i3*10+i4)*(i1*10+i2)==i) System.out.println(i + " = " + i3 + i4 + " * " + i1 + i2);
+				else if ((i3*10+i4)*(i2*10+i1)==i) System.out.println(i + " = " + i3 + i4 + " * " + i2 + i1);
+				}
+				
+			} 
+		}
---
+++ src/chapter3/Exercise8.java
@@ -0,0 +1,19 @@
+package chapter3;
+/* Exercise 8: Create a switch statement that prints a message for each case, 
+ * and put the switch inside a for loop that tries each case. 
+ * Put a break after each case and test it, then remove the breaks and see what happens.
+ */
+public class Exercise8 {
+	public static void main(String[] args){
+		for(int i=0; i<100; i++){
+			switch(i){
+			case 33 : System.out.println("Thirty three"); // break;
+			case 45 : System.out.println("Fourty five"); // break;
+			case 75 : System.out.println("Seventy five"); // break;
+			case 91 : System.out.println("Ninety one"); // break;
+			default : System.out.println("Other");
+			}
+				 
+		}
+	}
+}
