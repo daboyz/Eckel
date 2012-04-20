bbecf7cd0bac315c638549e384411f8c7d288510
Anton Shashuk <humaninterfaced@gmail.com>
2012-04-20 18:34:21 +0300

    ch8 ex1 draft

---
+++ src/chapter8_Interfaces/Exercise1.java
@@ -0,0 +1,8 @@
+package chapter8_Interfaces;
+// Exercise 1: Modify Exercise 9 in the previous chapter so that Rodent is an abstract class. Make the methods of Rodent abstract whenever possible.
+
+public class Exercise1 {
+	public static void main(String[] args) {
+		System.out.println(" ");
+	}
+}
