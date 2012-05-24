965dc243f21e7cf68e3f19f456960839463e7d63
Anton Shashuk <humaninterfaced@gmail.com>
2012-05-24 16:16:56 +0300

    ch8 ex18-19

--- src/chapter8_Interfaces/Exercise18.java
+++ src/chapter8_Interfaces/Exercise18.java
@@ -1,8 +1,57 @@
 package chapter8_Interfaces;
 // Exercise 18: Create a Cycle interface, with implementations Unicycle, Bicycle and Tricycle. Create factories for each type of Cycle, and code that uses these factories.
+interface Cycle{
+	void ride();
+}
+interface CycleFactory{
+	Cycle getCycle();
+}
 
-public class Exercise18 {
-	public static void main(String[] args) {
-		System.out.println();
+class Unicycle implements Cycle{
+	public void ride(){
+		System.out.println("Riding Uni");
 	}
 }
+class UniFactory implements CycleFactory{
+	public Cycle getCycle(){
+		return new Unicycle();
+	}
+}
+
+class Bicycle implements Cycle{
+	public void ride(){
+		System.out.println("Riding Bi");
+	}
+}
+class BiFactory implements CycleFactory{
+	public Cycle getCycle(){
+		return new Bicycle();
+	}
+}
+
+class Tricycle implements Cycle{
+	public void ride(){
+		System.out.println("Riding Tri");
+	}
+}
+class TriFactory implements CycleFactory{
+	public Cycle getCycle(){
+		return new Tricycle();
+	}
+}
+
+public class Exercise18 {
+	public static void rideCycle(CycleFactory factory){
+		Cycle c = factory.getCycle();
+		c.ride();
+	}
+	public static void main(String[] args) {
+		rideCycle(new UniFactory());
+		rideCycle(new BiFactory());
+		rideCycle(new TriFactory());
+	}
+}/* Output:
+Riding Uni
+Riding Bi
+Riding Tri
+*/
--- src/chapter8_Interfaces/Exercise19.java
+++ src/chapter8_Interfaces/Exercise19.java
@@ -1,7 +1,51 @@
 package chapter8_Interfaces;
+
+import java.util.Random;
 // Exercise 19: Create a framework using Factory Methods that performs both coin tossing and dice tossing.
-public class Exercise19 {
-	public static void main(String[] args) {
-		System.out.println();
+
+interface Tosser{
+	void toss();
+}
+interface TosserFactory{
+	Tosser getTosser();
+}
+
+class Coin implements Tosser{
+	public void toss(){
+		Random rand = new Random(42);
+		if (rand.nextInt(1)==1)
+		System.out.println("Coin tossed for Heads");
+		else System.out.println("Coin tossed for Tails");
 	}
 }
+class CoinFactory implements TosserFactory{
+	public Tosser getTosser(){
+		return new Coin();
+	}
+}
+
+class Dice implements Tosser{
+	public void toss(){
+		Random rand = new Random(42);
+		System.out.println("Dice tossed for " + (rand.nextInt(5)+1));
+	}
+}
+class DiceFactory implements TosserFactory{
+	public Tosser getTosser(){
+		return new Dice();
+	}
+}
+
+public class Exercise19 {
+	public static void tossTosser(TosserFactory factory){
+		Tosser t = factory.getTosser();
+		t.toss();
+	}
+	public static void main(String[] args) {
+		tossTosser(new CoinFactory());
+		tossTosser(new DiceFactory());
+	}
+}/* Output:
+Coin tossed for Tails
+Dice tossed for 1
+*/
