c10e9c44724e9fca88b3828ed89a1f88615985b5
Anton Shashuk <humaninterfaced@gmail.com>
2012-05-24 16:45:26 +0300

    ch9 ex1

---
+++ src/chapter9/Exercise1.java
@@ -0,0 +1,23 @@
+package chapter9;
+/* Exercise 1: Write a class named Outer that contains an inner class named Inner. 
+ * Add a method to Outer that returns an object of type Inner. In main( ), create and initialize a reference to an Inner.
+ */
+class Outer{
+	class Inner{
+		Inner(){
+			System.out.println("Inner class created");
+		}
+	}
+	public Inner getInner(){
+		return new Inner();
+	}
+}
+
+public class Exercise1 {
+	public static void main(String[] args) {
+		Outer outer = new Outer();
+		outer.getInner();
+	}
+}/* Output:
+Inner class created
+*/
